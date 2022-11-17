ed8318a19ecbdbdb6ae5bd80c21ed4079cca3a89
MrMace <mrmace@iu.edu>
2022-11-17 21:57:42 +0000

    Updated Js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
+const anchor = document.getElementById('anchor');
+const rekt = anchor.getBoundingClientRect();
+const anchorX = (rekt.left + rekt.width / 2);
+const anchorY = (rekt.top + rekt.height / 2) - 360;
+const eyes = document.querySelectorAll('.eye')
+
 document.addEventListener('mousemove', (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
-    const anchor = document.getElementById('anchor');
-    const rekt = anchor.getBoundingClientRect();
-    const anchorX = rekt.left + rekt.width / 2;
-    const anchorY = (rekt.top + rekt.height / 2) - 360;
     const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+  
     console.log(angleDeg);
-    const eyes = document.querySelectorAll('.eye')
+   
     eyes.forEach(eye => {
         eye.style.transform = `rotate(${90 + angleDeg}deg)`;
-        anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
-    })
+      anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
+    } )
 
 });
 
